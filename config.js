a70a50386476bd85ae1900cce11f26d5c5da194d
William Kurosawa <wkurosawa@gmail.com>
2018-01-31 15:16:55 +0900

    Add Google Analytics tracking ID

--- config.js
+++ config.js
@@ -18,4 +18,5 @@ module.exports = {
   recaptchaSecretKey: process.env.RECAPTCHA_SECRET || null,
   // default locale
   locale: process.env.LOCALE || "en",
+  gaTrackingid: process.env.GA_TRACKING_ID || null,
 };
